60f970aef88de9d89bded76418f63b9a7536726c
mcahyadi-kinoite <mcahyadit@github.com>
2025-09-28 00:28:08 +0800

    v2.3.0.5
    - Tidied up the flake.nix
    - So far kind of works when installed in a host machine
    - ..but breaks inside a Toolbox. Might need more testing

--- flake.nix
+++ flake.nix
@@ -1,59 +1,78 @@
 {
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+  description = "Development Shell with Nix if you don't know what to install";
 
-  outputs = { self, nixpkgs }:
-    let
-      projectName = "HoloSimpCart-NET";
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-      podmanSetupScript = let
-        registriesConf = pkgs.writeText "registries.conf" ''
-          [registries.search]
-          registries = [ 'docker.io' ]
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-          [registries.block]
-          registries = []
-        '';
-      in pkgs.writeScript "podman-setup" ''
-        #!${pkgs.runtimeShell}
-        policy="$XDG_CONFIG_HOME/containers/policy.json"
-        registries="$XDG_CONFIG_HOME/containers/registries.conf"
+        #=================
+        # Podman-Docker fix
+        #=================
+        # There seems to have some isses with permission and daemon
+        # ..with nix, if so try uncommenting code below
 
-        if ! test -f "$policy"; then
-          install -Dm555 ${pkgs.skopeo.src}/default-policy.json "$policy"
-        fi
+        #=================
+        # podmanSetupScript = let
+        #   registriesConf = pkgs.writeText "registries.conf" ''
+        #     [registries.search]
+        #     registries = [ 'docker.io' ]
+        #
+        #     [registries.block]
+        #     registries = []
+        #   '';
+        # in pkgs.writeScript "podman-setup" ''
+        #   #!${pkgs.runtimeShell}
+        #   policy="$XDG_CONFIG_HOME/containers/policy.json"
+        #   registries="$XDG_CONFIG_HOME/containers/registries.conf"
+        #
+        #   if ! test -f "$policy"; then
+        #     install -Dm555 ${pkgs.skopeo.src}/default-policy.json "$policy"
+        #   fi
+        #
+        #   if ! test -f "$registries"; then
+        #     install -Dm555 ${registriesConf} "$registries"
+        #   fi
+        # '';
+        #
+        # dockerCompat = pkgs.runCommandNoCC "docker-podman-compat" { } ''
+        #   mkdir -p $out/bin
+        #   ln -s ${pkgs.podman}/bin/podman $out/bin/docker
+        # '';
+        #=================
+        # Then add this in shellHook
+        #=================
+        # ${podmanSetupScript}
+        # export BUILDAH_ISOLATION=chroot
+        # export CGROUP_MANAGER=cgroupfs
+        #=================
+        # And this inside packages:
+        # dockerCompat
 
-        if ! test -f "$registries"; then
-          install -Dm555 ${registriesConf} "$registries"
-        fi
-      '';
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            # dockerCompat
 
-      dockerCompat = pkgs.runCommandNoCC "docker-podman-compat" { } ''
-        mkdir -p $out/bin
-        ln -s ${pkgs.podman}/bin/podman $out/bin/docker
-      '';
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          dockerCompat
+            pkgs.dotnet-sdk
+            pkgs.dotnet-ef
 
-          pkgs.dotnet-sdk
-          pkgs.dotnet-ef
+            pkgs.podman
+            pkgs.podman-compose
+          ];
 
-          pkgs.podman
-          pkgs.podman-compose
-
-          pkgs.nushell
-          # pkgs.fuse-overlayfs
-        ];
-
-        shellHook = ''
-          ${podmanSetupScript}
-          export BUILDAH_ISOLATION=chroot
+          # ${podmanSetupScript}
+          shellHook = ''
+          # export BUILDAH_ISOLATION=chroot
           # export CGROUP_MANAGER=cgroupfs
           echo "You are using pre-configured Nix Shell for HoloSimpCart-NET"
-        '';
-      };
-    };
+          '';
+        };
+      }
+    );
 }
