e4bf3c621c78a4a8c2acb432ff785c1ff50f22c4
mcahyadit-jj <mcahyadit-jj@github.com>
2025-11-06 20:33:33 +0800

    formatted flake.nix

--- flake.nix
+++ flake.nix
@@ -7,11 +7,18 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             # C#
