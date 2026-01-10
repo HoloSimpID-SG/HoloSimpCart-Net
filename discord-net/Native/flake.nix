b502c0cc1a7c28cef312ef8ce13ae61e4a9f14cb
mcahyadit <mcahyadit@github.com>
2026-01-10 16:20:47 +0800

    chore(flake): tweaked flake for discord-net/Native, adding zon2nix

--- discord-net/Native/flake.nix
+++ discord-net/Native/flake.nix
@@ -1,32 +1,62 @@
 {
   inputs = {
     self.submodules = true;
-    nixpkgs.url = "github:NixOS/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    zig = {
+      url = "github:mitchellh/zig-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        flake-compat.follows = "flake-compat";
+      };
+    };
+    zon2nix = {
+      url = "github:jcollie/zon2nix?rev=bf983aa90ff169372b9fa8c02e57ea75e0b42245";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    zig,
+    zon2nix,
+    ...
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "libNative";
+          version = "1.0";
+          src = ./.;
+          postPatch = ''
+            ln -s ${pkgs.callPackage ./build.zig.zon.nix {}} $ZIG_GLOBAL_CACHE_DIR/p
+          '';
+          buildPhase = ''
+            just native
+          '';
+          installPhase = ''
+            just install-native
+          '';
+        };
         devShellFragments.default = {
-          packages = with pkgs; [
-            zig
-            zls
-            clang-tools
+          packages =
+            (with pkgs; [
+              zig
+              zls
+              clang-tools
 
-            nixd
-            alejandra
-          ];
+              nixd
+              alejandra
+            ])
+            ++ zon2nix.packages.${system}.zon2nix;
         };
         devShells = pkgs.mkShellNoCC self.devShellFragments.${system}.default;
       }
