771d393749be85699a014b0127e879b159f183d4
mcahyadit <mcahyadit@github.com>
2026-02-02 22:24:43 +0800

    fix(manim): fixed manim not showing fonts due to alpine minimal packages

--- python-uvicorn/app/services/manim_text.py
+++ python-uvicorn/app/services/manim_text.py
@@ -3,36 +3,38 @@ from fastapi import APIRouter
 from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
 from manim import Scene, Text, Write, config
+from manim_fonts import RegisterFont
 
 
 class TextScene(Scene):
-    def __init__(self, text, **kwargs):
-        self.text_to_render = text
-        super().__init__(**kwargs)
+  def __init__(self, text, **kwargs):
+    self.text_to_render = text
+    super().__init__(**kwargs)
 
-    def construct(self):
-        text = Text(self.text_to_render, font_size=72)
-        self.play(Write(text))
-        self.wait(1)
+  def construct(self):
+    with RegisterFont("Roboto") as fonts:
+      text = Text(self.text_to_render, font_size=72, font=fonts[0])
+      self.play(Write(text))
+      self.wait(1)
 
 
 def _manim_text(input_text: str) -> str:
-    config.media_dir = os.path.join("/shared", "manim_media")
-    config.quality = "low_quality"
+  config.media_dir = os.path.join("/shared", "manim_media")
+  config.quality = "low_quality"
 
-    scene = TextScene(input_text)
-    scene.render()
+  scene = TextScene(input_text)
+  scene.render()
 
-    return str(scene.renderer.file_writer.movie_file_path)
+  return str(scene.renderer.file_writer.movie_file_path)
 
 
 router = APIRouter()
 
 
 class Input(BaseModel):
-    text: str
+  text: str
 
 
 @router.post("/manim_text", response_class=PlainTextResponse)
 def manim_text(input: Input) -> str:
-    return _manim_text(input.text)
+  return _manim_text(input.text)
